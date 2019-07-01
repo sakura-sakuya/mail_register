75b2841778c081cb19f41d75c639ba3d77f21430
sakura-sakuya <854220036@qq.com>
2019-07-01 16:45:58 +0800

    Initial commit

---
+++ login_web/src/main/java/com/sui/login_web/LoginWebApplication.java
@@ -0,0 +1,13 @@
+package com.sui.login_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.sui")
+public class LoginWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginWebApplication.class, args);
+	}
+
+}
